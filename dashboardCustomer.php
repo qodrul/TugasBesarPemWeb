75aaab1d83d59727d33f2ce661e710608ebda29d
KhaDjoe <djoe.khairan@gmail.com>
2026-06-22 17:52:03 +0800

    set up font

--- dashboardCustomer.php
+++ dashboardCustomer.php
@@ -5,3 +5,19 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] != 'customer') {
     exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Customer | WeCleanIt</title>
+
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Syne:wght@500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+    
+</body>
+</html>
